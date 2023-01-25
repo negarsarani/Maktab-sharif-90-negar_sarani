1cb4e39a13c4b606817f1c4b0ec4bf5d81df4708
Negar Sarani <negar.sarani2000@gmail.com>
2023-01-26 01:43:52 +0330

    this is for question5

---
+++ Q5/app.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const ask = (question, yes, no) => {
+    
+    if (confirm(question)) yes();
+    else no();
+}
+ask("Do you agree?",
+    () => { alert("You agreed."); },
+    () => { alert("You canceled the execution."); }
+);
